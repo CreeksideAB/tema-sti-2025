e8685c40bf52c5d3345b455e9c4f98ba336efe46
TheTecGuy05 <rtm1542@gmail.com>
2025-01-23 14:44:18 +0100

    Added DB

--- Embedded/main/include/boking_3.h
+++ Embedded/main/include/boking_3.h
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+#ifndef SPACE_DESTINATIONS_H
+#define SPACE_DESTINATIONS_H
 void start_task_group3();
 
 void task_group3(void* params);
+
+const char DB = {"Mars", "Jupiter", "Saturnus", "Venus"};
+
