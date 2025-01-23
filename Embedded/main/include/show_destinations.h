aacc45897d1b1f841d6595ccddd33f31806cb358
David <stenmandavid03@gmail.com>
2025-01-23 15:10:33 +0100

    Group1: Add travel handling WIP

--- Embedded/main/include/show_destinations.h
+++ Embedded/main/include/show_destinations.h
@@ -1,6 +1,7 @@
 #ifndef SHOW_DESTINATIONS_H
 #define SHOW_DESTINATIONS_H
 
+char *read_file(const char *filename);
 void check_availability(char* json_input);
 void show_destinations_task(void *pvParameters);
 void start_task_group1();
--- Embedded/main/show_destinations.c
+++ Embedded/main/show_destinations.c
@@ -3,9 +3,39 @@
 #include "freertos/task.h"
 #include "cJSON.h"
 
-void check_availability(char* json_input) {
-    
+
+// Funktion för att läsa en fil till en buffer
+char *read_file(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Kunde inte öppna filen");
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long filesize = ftell(file);
+    rewind(file);
+
+    char *buffer = (char *)malloc(filesize + 1);
+    if (!buffer) {
+        perror("Kunde inte allokera minne");
+        fclose(file);
+        return NULL;
+    }
+
+    fread(buffer, 1, filesize, file);
+    buffer[filesize] = '\0';
+
+    fclose(file);
+    return buffer;
 }
+
+void check_availability(char* json_input) {
+
+    // TODO: Kolla vilka destinationer som är tillgängliga det datumet
+
+}
+
 
 void show_destinations_task(void *pvParameters) {
 
