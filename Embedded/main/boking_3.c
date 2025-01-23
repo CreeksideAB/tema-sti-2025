afd0469cd462931eadccf792ff8c2dc25f42a96e
JesperMorais <jeppe.morris@hotmail.se>
2025-01-23 14:41:05 +0100

    added mqtt functiojs

--- Embedded/main/boking_3.c
+++ Embedded/main/boking_3.c
@@ -2,16 +2,24 @@
 #include <freeRTOS./FreeRTOS.h>
 #include <freertos/task.h>
 #include <freeRTOS/event_groups.h>
+#include <freeRTOS/queue.h>
+
+QueueHandle_t xQueue;
 
 void start_task_group3()
 {
     printf("Starting task group 3\n");
     xTaskCreate(&task_group3, "Funktion", 2048, NULL, 5, NULL);
-    
+    printf("Task group 3 started\n");
 }
 
 
 void task_group3(void* params)
 {
-    
+    // send message to mqtt
+    //TODO FIX DATABASE
+    while(true){
+        send_data_to_mqtt("Hello from task 3");
+        vTaskDelay(pdMS_TO_TICKS(5000));
+    }   
 }
